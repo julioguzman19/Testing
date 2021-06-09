ae52ca97092c5c450a477af5b1e83a486b7f5106
Julio <julioguzman19@gmail.com>
2021-06-09 08:31:14 -0600

    Changing html to form

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,6 @@
 
-$('#button').on("input", function () {
-    let businessSearch = document.getElementById("inputSearch").value;
+$('#myInput').on("input", function () {
+    let businessSearch = document.getElementById("myInput").value;
     let api = "http://www.mapquestapi.com/search/v3/prediction?";
     let apiKey = "key=A7A5TP3tsSvq4IqcXRmXxVNa6e8JHbA4";
     let query = `&limit=5&collection=adminArea,poi,address,category,franchise,airport&q=${businessSearch}`;
@@ -14,15 +14,9 @@ $('#button').on("input", function () {
             console.log(response);
             var countries = ["Afghanistan","Albania"];
             autocomplete(businessSearch, countries);
-            // let headline = response.response.docs[i].headline.main;
-            // let articleUrl = response.response.docs[i].web_url;
+ 
             // let link = articleUrl.link(articleUrl); //turning URLs to clickable links for user
-            // let snippet = response.response.docs[i].snippet;
-
-            // $('#newHeadlineOnSearch').append(i + 1 + ": " + headline);
-            // $('#newHeadlineOnSearch').append("<p></p>");
-            // $('#newSnippetsOnSearch').append(i + 1 + ": " + snippet);
-            // $('#newSnippetsOnSearch').append("<p></p>");
+ 
             // $('#newNewsLinksOnSearch').append(i + 1 + ": " + link);
             // $('#newNewsLinksOnSearch').append("<p></p>");
         })
